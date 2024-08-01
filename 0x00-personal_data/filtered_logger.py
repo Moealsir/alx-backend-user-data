94e97527a3e94cca8930e3162e03f1df9ed0dd7d
Moealsir <mohamedwdalsir@gmail.com>
2024-08-02 02:37:32 +0300

    Task 1: filtered_logger

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,6 +2,26 @@
 """ Use of regex in replacing occurrences of certain field values """
 import re
 
+import logging
+from typing import List
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        return filter_datum(self.fields, self.REDACTION,
+                            super().format(record), self.SEPARATOR)
+
 
 patterns = {
     'extract': lambda x, y: r'(?P<field>{})=[^{}]*'.format('|'.join(x), y),
@@ -10,7 +30,7 @@ patterns = {
 
 
 def filter_datum(
-        fields: list[str], redaction: str, message: str, separator: str,
+        fields: List[str], redaction: str, message: str, separator: str,
         ) -> str:
     """Filters a log line.
     """
