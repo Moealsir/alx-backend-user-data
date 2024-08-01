6cc10f994cd18a1ea7c29c1245ab584f5b100541
Moealsir <mohamedwdalsir@gmail.com>
2024-08-02 02:52:03 +0300

    Task 2: add logger

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
-""" Use of regex in replacing occurrences of certain field values """
+"""A module for filtering logs.
+"""
+import os
 import re
 import logging
 from typing import List
@@ -9,25 +11,7 @@ patterns = {
     'extract': lambda x, y: r'(?P<field>{})=[^{}]*'.format('|'.join(x), y),
     'replace': lambda x: r'\g<field>={}'.format(x),
 }
-
 PII_FIELDS = ("name", "email", "phone", "ssn", "password")
-
-
-class RedactingFormatter(logging.Formatter):
-    """ Redacting Formatter class
-        """
-
-    REDACTION = "***"
-    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
-    SEPARATOR = ";"
-
-    def __init__(self, fields: List[str]):
-        super(RedactingFormatter, self).__init__(self.FORMAT)
-        self.fields = fields
-
-    def format(self, record: logging.LogRecord) -> str:
-        return filter_datum(self.fields, self.REDACTION,
-                            super().format(record), self.SEPARATOR)
 
 
 def filter_datum(
@@ -39,12 +23,34 @@ def filter_datum(
     return re.sub(extract(fields, separator), replace(redaction), message)
 
 
-def get_logger():
-    """ Create a new logger for user data """
+def get_logger() -> logging.Logger:
+    """Creates a new logger for user data.
+    """
     logger = logging.getLogger("user_data")
-    handler = logging.StreamHandler()
-    handler.setFormatter(RedactingFormatter(PII_FIELDS))
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(RedactingFormatter(PII_FIELDS))
     logger.setLevel(logging.INFO)
     logger.propagate = False
-    logger.addHandler(handler)
+    logger.addHandler(stream_handler)
     return logger
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+    """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    FORMAT_FIELDS = ('name', 'levelname', 'asctime', 'message')
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """formats a LogRecord.
+        """
+        msg = super(RedactingFormatter, self).format(record)
+        txt = filter_datum(self.fields, self.REDACTION, msg, self.SEPARATOR)
+        return txt
