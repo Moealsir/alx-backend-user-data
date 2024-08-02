3fab922ddb84d480115e5000b264ffd3a98594dd
Moealsir <mohamedwdalsir@gmail.com>
2024-08-02 03:06:06 +0300

    Task 3: Connect to secure database

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -5,6 +5,7 @@ import os
 import re
 import logging
 from typing import List
+import mysql.connector
 
 
 patterns = {
@@ -33,6 +34,22 @@ def get_logger() -> logging.Logger:
     logger.propagate = False
     logger.addHandler(stream_handler)
     return logger
+
+
+def get_db() -> mysql.connector.MySQLConnection:
+    """ Create a database connector """
+    db_host = os.getenv("PERSONAL_DATA_DB_HOST", "localhost")
+    db_name = os.getenv("PERSONAL_DATA_DB_NAME", "")
+    db_user = os.getenv("PERSONAL_DATA_DB_USERNAME", "root")
+    db_pwd = os.getenv("PERSONAL_DATA_DB_PASSWORD", "")
+    conn = mysql.connector.connect(
+        host=db_host,
+        port=3306,
+        user=db_user,
+        password=db_pwd,
+        database=db_name
+    )
+    return conn
 
 
 class RedactingFormatter(logging.Formatter):
